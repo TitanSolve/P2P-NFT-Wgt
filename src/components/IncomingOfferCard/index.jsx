9141cb64ad745bb69736201e5db1642b9c2f2537
TitanSolve <asakurar31@gmail.com>
2025-09-05 10:17:31 +0400

    update backend url

--- src/components/IncomingOfferCard/index.jsx
+++ src/components/IncomingOfferCard/index.jsx
@@ -149,20 +149,21 @@ const IncomingOfferCard = ({
       ws.onmessage = (event) => {
         const data = JSON.parse(event.data);
         if (data.signed) {
-          // const requestBody = {
-          //   account: myWalletAddress,
-          // };
-          // const response = fetch(
-          //   `${API_URLS.backendUrl}/deduct-mCredit`,
-          //   {
-          //     method: "POST",
-          //     headers: {
-          //       "Content-Type": "application/json",
-          //     },
-          //     body: JSON.stringify(requestBody),
-          //   }
-          // );
-          // console.log("deduction result:", response);
+          const requestBody = {
+            account: myWalletAddress,
+            offerType: "accept_transfer_offer",
+          };
+          const response = fetch(
+            `${API_URLS.backendUrl}/deduct-mCredit`,
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify(requestBody),
+            }
+          );
+          console.log("deduction result:", response);
 
           setTransactionStatus("Transaction signed");
           setIsQrModalVisible(false);
